8c988a7c9687bbfa688cc7e24bf0e6c32a121d7d
ankitsingh <ankitsingh4634@gmail.com>
2023-08-03 17:28:35 +0530

    my changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTodo = () => {
+    if (inputValue.trim() === "") {
+      return;
+    }
+
+    if (todos.includes(inputValue)) {
+      alert("This value already exists in the list.");
+      return;
+    }
+
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h1>Todo App</h1>
       </header>
+      <div className="todo-container">
+        <div className="todo-input">
+          <input type="text" value={inputValue} onChange={handleInputChange} />
+          <button onClick={addTodo}>Add Todo</button>
+        </div>
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+            <div key={index} className="todo-item">
+              <span>{todo}</span>
+              <button onClick={() => removeTodo(index)}>Remove</button>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
